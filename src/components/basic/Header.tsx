fdf2c72d914e709cbab14717aefc1481b8b068dd
Peterlits Zo <peterlitszo@gmail.com>
2022-04-26 18:58:02 +0800

    style: Fix typo for component `Header`

--- src/components/basic/Header.tsx
+++ src/components/basic/Header.tsx
@@ -2,10 +2,10 @@ import type { Component } from 'solid-js';
 
 import styles from './styles/Header.module.css';
 
-const Left: Component = ({children}) => {
+const Header: Component = ({children}) => {
   return (
     <h2 class={styles.header}>{children}</h2>
   );
 }
 
-export default Left;
+export default Header;
